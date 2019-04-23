a068099b1ba792b743b8885edb69400df1b9dc52
Juan Pablo Villa <jpvilla1990@gmail.com>
2019-04-23 21:46:29 +0200

    .

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,71 @@
 import React from 'react';
+import '../css/styles.css';
+
+
+class Header extends React.Component{
+
+    constructor(props){
+        super(props)
+        this.state = {
+            active:false,
+            keywords:'Hello'
+        }
+    }
+
+
+
+inputChangeHandler1(){
+    console.log("I was clicked")
+}
+
+inputChangeHandler2=(event)=>{
+    //console.log(event.target.value);
+    const value = event.target.value === '' ? false:true;
+    this.setState({
+        active:value,
+        keywords:event.target.value
+    })
+}
+
+    render(){
+        console.log(this.state.keywords);
+
+        let backgroundHeader = 'red';
+
+        if(this.state.keywords!==''){
+            backgroundHeader='blue';
+        }else{
+            backgroundHeader='red'
+        }
+/*const styles = {
+    header:{
+        background:'#03a9f4'
+    },
+    logo:{
+        color:'#fff',
+        fontFamily:'Anton',
+        textAlign:'center'
+    }
+}*/
+
+       /* return (
+    <header style={styles.header}>
+        <div style={styles.logo}>logo</div>
+        <input type="text"/>
+    </header>
+    )
+    }*/
+
+          return (
+    <header style={{background:`${this.state.active ? 'blue':'red'}`}}>
+        <div className="logo" 
+        onClick={this.inputChangeHandler1}>Logo</div>
+        <input type="text" onChange={(e) => {this.inputChangeHandler2(e)}}/>
+    </header>
+    )
+    }
+
+}
 
 const user = {
     name:'Juan',
@@ -11,16 +78,17 @@ const getYear = () =>{
     return newDate.getFullYear();
 }
 
-const Header = () =>{
+/*const Header = () =>{
     return <div>The date is {
         getYear()
     }
+   
     <div>
         <div>{user.name}</div>
         <div>{user.lastname}</div>
         <div>{user.age}</div>
     </div>
         </div>
-}
+}*/
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
+import JSON from './db.json';
 
 import Header from './components/header';
+import NewsList from './components/news_list';
 
-const App = () => {
+class App extends Component {
+
+state={
+    news:JSON
+}
+
+    render(){
     return <div>
         <Header/>
+        <NewsList news={this.state.news}/>
     </div>;
     //return React.createElement('h1',{className:'title'},React.createElement('div',{className:'title'},'Hello World'));
-}/*
+}
+}
+/*
+
 const App2 = () => {
     return(
     <div className="newElement">
